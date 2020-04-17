65d49267361acace2262d73cdc9ba749e29a48ad
donna-marijne <donnamarijne+gh@gmail.com>
2020-04-17 10:50:45 +0100

    Release 2.0.0 (#17)
    * Add release notes.
    * Remove prerelease changelog entries, since they will no longer be useful to consumers. (They can still be found on the GitHub Releases page.)
    * Remove `VERSION_PRERELEASE` from pom.xml, this will ensure that the release version is 2.0.0.

--- docs/app/pages/changelog/changelog.component.ts
+++ docs/app/pages/changelog/changelog.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
 import { IChangeLog } from '../../interfaces/IChangeLog';
-import { AppConfiguration } from '../../services/app-configuration/app-configuration.service';
 
 @Component({
     selector: 'uxd-changelog-page',
@@ -12,45 +10,14 @@ export class ChangeLogPageComponent {
 
     logs: IChangeLog[];
 
-    constructor(domSanitizer: DomSanitizer, appConfig: AppConfiguration) {
+    constructor() {
 
         this.logs = [
             {
-                version: '2.0.0-rc.1',
-                date: 'February 7th 2020',
-                content: require('./logs/release-v2.0.0-rc.1.md')
-            },
-            {
-                version: '2.0.0-beta.5',
-                date: 'August 10th 2019',
-                content: require('./logs/release-v2.0.0-beta.5.md')
-            },
-            {
-                version: '2.0.0-beta.4',
-                date: 'August 20th 2019',
-                content: require('./logs/release-v2.0.0-beta.4.md')
-            },
-            {
-                version: '2.0.0-beta.3',
-                date: 'July 11th 2019',
-                content: require('./logs/release-v2.0.0-beta.3.md')
-            },
-            {
-                version: '2.0.0-beta.2',
-                date: 'June 27th 2019',
-                content: require('./logs/release-v2.0.0-beta.2.md')
-            },
-            {
-                version: '2.0.0-beta.1',
-                date: 'June 12th 2019',
-                content: require('./logs/release-v2.0.0-beta.1.md')
+                version: '2.0.0',
+                date: 'April 17th 2020',
+                content: require('./logs/release-v2.0.0.md')
             }
         ];
-
-        this.logs.forEach(log => {
-            const markdown = log.content.replace(/{{baseUrl}}/g, appConfig.baseUrl);
-            log.content = domSanitizer.bypassSecurityTrustHtml(markdown) as string;
-        });
-
     }
 }
--- release-notes-2.0.0.md
+++ release-notes-2.0.0.md
@@ -1,11 +1,12 @@
-UX Aspects (AngularJS) 2.0.0 release candidate 1 is now available! Check out the documentation at [uxaspects.github.io/UXAspects-ng1](https://uxaspects.github.io/UXAspects-ng1).
+UX Aspects (AngularJS) 2.0.0 is now available! Check out the documentation at [uxaspects.github.io/UXAspects-ng1](https://uxaspects.github.io/UXAspects-ng1).
 
 #### Installation
 ````bash
-npm install @ux-aspects/ux-aspects-ng1@2.0.0-rc.1
+npm install @ux-aspects/ux-aspects-ng1@2.0.0
 ````
 
 #### Breaking Changes (from UX Aspects 1.x)
+Previous changes can be found in the [UX Aspects changelog](https://uxaspects.github.io/UXAspects/#/changelog).
 * Import paths for AngularJS scripts, stylesheets, fonts, and images must be updated to refer to `@ux-aspects/ux-aspects-ng1`.
 * `HybridModule` must be imported from `@ux-aspects/ux-aspects-ng1`.
 * License information for embedded dependencies is contained in the file `3rd-party-licenses.txt` inside the `@ux-aspects/ux-aspects-ng1` package.
